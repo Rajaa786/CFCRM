b011b6165c3788d26eed3e1cf5a5c3b1f0c23887
Sanchay Thalnerkar <thalnerkarsanchay17@gmail.com>
2022-10-08 20:02:54 +0530

    Revert "Final"
    This reverts commit fa2590aeb0d59b1377e438876f7c05aab94a4447.

--- account/urls.py
+++ account/urls.py
@@ -5,11 +5,16 @@ from .views import VerificationView
 from django.conf.urls.static import static
 from django.conf import settings
 
-app_name = 'account'
+
+app_name = "account"
 
 # app_name = "accounts"
 urlpatterns = [
     path('dashboard/', views.base_dashboard, name="base_dashboard"),
+
+    # app_name = "accounts"
+
+
 
     #--------------------------------------------------------#
     # new_LEADS
