d86d2459583b387af72df3ea4afa2502c7d8bddf
Sanchay Thalnerkar <thalnerkarsanchay17@gmail.com>
2022-10-08 21:34:58 +0530

    ye bhi chal rha hai

--- account/urls.py
+++ account/urls.py
@@ -42,7 +42,7 @@ urlpatterns = [
     path('register', views.register, name="register"),
     path('login/', views.login, name="login"),
     path('logout', views.logout, name="logout"),
-    #     path('deleteapplicant/<int:id>', views.delapplicant, name="deleteapplicant"),
+    path('deleteapplicant/<int:id>', views.delapplicant, name="deleteapplicant"),
     path('editapplicant', views.editapplicant, name="edit_applicant"),
 
     path('reset_password/', auth_views.PasswordResetView.as_view(
